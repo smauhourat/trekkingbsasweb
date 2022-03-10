b46e5711dc5a42f0e8778dbbefe70eba73df58c3
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-03-10 17:29:02 -0300

    add splide lib

---
+++ js/splide.js
@@ -0,0 +1,15 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+
+  var splide = new Splide( '.splide' );
+  var bar    = splide.root.querySelector( '.my-slider-progress-bar' );
+  
+  // Update the bar width:
+  splide.on( 'mounted move', function () {
+    var end = splide.Components.Controller.getEnd() + 1;
+    bar.style.width = String( 100 * ( splide.index + 1 ) / end ) + '%';
+  } );
+  
+  splide.mount();
+
+} ) ;
+
